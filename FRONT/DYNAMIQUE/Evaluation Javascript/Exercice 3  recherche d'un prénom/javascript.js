a3619f55ea35c58942bb3f939be2d1dacbe91986
BehzadNekouei <behzad.nekouei1@gmail.com>
2021-02-19 11:16:03 +0100

    break lines added to the message

--- FRONT/DYNAMIQUE/Evaluation Javascript/Exercice 3  recherche d'un prénom/javascript.js
+++ FRONT/DYNAMIQUE/Evaluation Javascript/Exercice 3  recherche d'un prénom/javascript.js
@@ -12,7 +12,7 @@ var tab = [
 ];
 var t = false;
 var prenom = prompt(
-  "On recherche un prénom dans la list des prénom suivant: Audrey, Aurélien, Flavien, Jérémy, Laurent, Melik, Nouara, Salem, Samuel, Stéphane. Si le prénom est trouvé on l'élimine du tableau en décalant les cases qui le suivent et en mettant à blanc la dernière case. Si le prénom n'est pas trouvé un message d'erreur apparait et aucun prénom ne se supprime. Veillez saisir un prénom: "
+  "On recherche un prénom dans la list des prénom suivant:\n Audrey\n Aurélien\n Flavien\n Jérémy\n Laurent\n Melik\n Nouara\n Salem\n Samuel\n Stéphane\n Si le prénom est trouvé on l'élimine du tableau en décalant les cases qui le suivent et en mettant à blanc la dernière case. Si le prénom n'est pas trouvé un message d'erreur apparait et aucun prénom ne se supprime. Veillez saisir un prénom: "
 );
 for (var i = 0; i < 10; i++) {
   if (tab[i] == prenom) {
