d7a7afcc9ef01248365129b44d9ab2b84a32352c
BehzadNekouei <behzad.nekouei1@gmail.com>
2021-03-03 09:13:10 +0100

    exercice 1,2,3 in php added

---
+++ BACK/SERVEUR/PHP/exercice-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
+   <b>Écrire un programme qui écrit 500 fois la phrase Je dois faire des sauvegardes régulières de mes fichiers.</b><br>
+   <?php
+   for ($i = 1; $i <= 500; $i++) {
+      echo "<p>Je dois faire des sauvegardes régulières de mes fichiers.</p>";
+   }
+   ?>
+</body>
+
+</html>
