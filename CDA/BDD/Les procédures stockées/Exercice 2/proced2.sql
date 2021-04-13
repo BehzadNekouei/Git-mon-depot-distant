38c5f09d1de2c9181b5ebf1377b13819e61d6611
beneko <behzadnekouei@gmail.com>
2021-04-13 09:57:19 +0200

    added file proced2.sql exrecice 2 BDD/views/procedure

---
+++ CDA/BDD/Les procédures stockées/Exercice 2/proced2.sql
@@ -0,0 +1,17 @@
+DELIMITER |
+CREATE PROCEDURE CA_Fournisseur(IN obs varchar(50))
+BEGIN
+    /*Sortir les produits des commandes ayant le mot "urgent' en observation?
+    (Afficher le numéro de commande, le nom du fournisseur,
+    le libellé du produit et le sous total= quantité commandée * Prix unitaire)*/
+    SELECT entcom.numcom AS 'le numéro de commande',
+    nomfou AS 'le nom du fournisseur', 
+    libart AS 'le libellé du produit' , 
+    (qtecde*priuni) AS 'le sous total'
+    FROM entcom,fournis,ligcom,produit
+    WHERE entcom.numfou=fournis.numfou
+    AND entcom.numcom=ligcom.numcom
+    AND produit.codart=ligcom.codart
+    AND obscom LIKE '%obs%';
+END |
+DELIMITER;
