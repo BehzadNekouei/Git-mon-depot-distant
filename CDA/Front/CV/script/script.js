9121b4c130e5de4060754b7efa2e0d4832103327
beneko <behzadnekouei@gmail.com>
2021-05-02 15:22:22 +0200

    CV -form validation with javascript improved with adding a fuction

--- CDA/Front/CV/script/script.js
+++ CDA/Front/CV/script/script.js
@@ -1,72 +1,67 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  // declare regex valariables to test with form inputs
   const validName = new RegExp(/^[A-Za-z]+$/);
-  const validMail = new RegExp(
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-  );
+  const validMail = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
   const validSujet = new RegExp(/^[A-Za-z]+$/);
   const validMessage = new RegExp(/[_a-z0-9-]+(\.[_a-z0-9-]+)*/);
-  // function to check the form
-  $("#submit").click(function () {
-    const name = $("#name").val();
-    const mail = $("#mail").val();
-    const sujet = $("#sujet").val();
-    const message = $("#message").val();
-    let bool = true;
-    const vide = "Veuillez renseigner ce champ.";
-    const invalid = "entrée invalide";
-    const i = '<i class="fas fa-exclamation-triangle"></i>';
-    // check if the name is correct!
-    if (name == "") {
-      $("#nameError").html(i + vide);
-      $(".name").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else if (!validName.test(name)) {
-      $("#nameError").html(i + invalid);
-      $(".name").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else {
-      $("#nameError").html("");
-      $(".name").removeClass("w3-panel w3-pale-red w3-padding-16");
+  // declare variables to take form inputs
+  const name = $('#name');
+  const mail = $('#mail');
+  const sujet = $('#sujet');
+  const message = $('#message');
+  // declare variables to take the elements for error messages
+  const nameError =  $('#nameError');
+  const mailError =  $('#mailError');
+  const sujetError =  $('#sujetError');
+  const messageError =  $('#messageError');
+  const i = '<i class="fas fa-exclamation-triangle"></i>';
+  // declare variables to check if every input is correct
+  let nameCheck, mailCheck , sujetCheck , messageCheck = false;
+  // function to valadate the form inputs
+  function validForm(input, regex , error){
+    // take the value of the form input
+    let value = input.val();
+    // take the grandparent of the input to add class
+    let grandParent = input.parent().parent();
+    // if the input value is note empty
+    if(!value == ''){
+      // if the input value is not match
+      if (!regex.test(value)){
+        // error message for incorect input
+        error.html(i+'entrée invalide.');
+        grandParent.addClass("w3-panel w3-pale-red w3-padding-16");
+        return false;
+      }
+      else {
+        // clear error 
+        error.text('');
+        grandParent.removeClass("w3-panel w3-pale-red w3-padding-16");
+        return true;
+      }
     }
-    // check if the mail is correct!
-    if (mail == "") {
-      $("#mailError").html(i + vide);
-      $(".mail").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else if (!validMail.test(mail)) {
-      $("#mailError").html(i + invalid);
-      $(".mail").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else {
-      $("#mailError").html("");
-      $(".mail").removeClass("w3-panel w3-pale-red w3-padding-16");
+    else {
+    // error message for empty input
+    error.html(i+'Veuillez renseigner ce champ.');
+    grandParent.addClass("w3-panel w3-pale-red w3-padding-16");
+    return false;
     }
-    // check if the subject is correct!
-    if (sujet == "") {
-      $("#sujetError").html(i + vide);
-      $(".sujet").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else if (!validSujet.test(sujet)) {
-      $("#sujetError").html(i + invalid);
-      $(".sujet").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else {
-      $("#sujetError").html("");
-      $(".sujet").removeClass("w3-panel w3-pale-red w3-padding-16");
+  }
+  // chech inputs on blur
+  name.blur(() => validForm(name, validName, nameError));
+  mail.blur(() => validForm(mail, validMail, mailError));
+  sujet.blur(() => validForm(sujet, validSujet, sujetError));
+  message.blur(() => validForm(message, validMessage, messageError));
+  // dcheck inputs on submit
+  $('#submit').click(function(){
+    nameCheck = validForm(name, validName, nameError);
+    mailCheck = validForm(mail, validMail, mailError);
+    sujetCheck = validForm(sujet, validSujet, sujetError);
+    messageCheck = validForm(message, validMessage, messageError);
+    if(nameCheck && mailCheck && sujetCheck && messageCheck){
+      return true;
     }
-    // check if the message is correct!
-    if (message == "") {
-      $("#messageError").html(i + vide);
-      $(".message").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else if (!validMessage.test(message)) {
-      $("#messageError").html(i + invalid);
-      $(".message").addClass("w3-panel w3-pale-red w3-padding-16");
-      bool = false;
-    } else {
-      $("#messageError").html("");
-      $(".message").removeClass("w3-panel w3-pale-red w3-padding-16");
+    else{
+      return false;
     }
-    return bool;
   });
 });
