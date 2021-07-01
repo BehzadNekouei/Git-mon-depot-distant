c02dcf697eadf5f4a1a5ca2fa89af4b95a159d0d
beneko <behzadnekouei@gmail.com>
2021-07-01 16:34:45 +0200

    CDA/front/vuejs: exercice calculatrice done

--- CDA/Front/Vuejs/Exercices/script.js
+++ CDA/Front/Vuejs/Exercices/script.js
@@ -34,23 +34,102 @@ var app2 = new Vue({
                     }
                 }
             } else {
-                this.message ='Veuillez entrer un chiffre entre 0 et 100'
+                this.message = 'Veuillez entrer un chiffre entre 0 et 100'
             }
         }
     }
 });
 
-var app3 =  new Vue({
-    el: '#app3',
-    data:{
-        num1: '',
-        num2: '',
-        operator: '',
-        result: ''
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        operand1: '0',
+        operand2: '0',
+        operator: null,
+        result: null,
+        display: '0'
     },
     methods: {
-        calc: function (){
+        calculate: function () {
+            if (!this.result) {
+                switch (this.operator) {
+                    case '+' :
+                        this.result = parseFloat(this.operand1) + parseFloat(this.operand2);
+                        this.display = parseFloat(this.result);
+                        break;
 
+                    case '-' :
+                        this.result = parseFloat(this.operand1) - parseFloat(this.operand2);
+                        this.display = parseFloat(this.result);
+                        break;
+
+                    case '*' :
+                        this.result = parseFloat(this.operand1) * parseFloat(this.operand2);
+                        this.display = parseFloat(this.result);
+                        break;
+
+                    case '/' :
+                        this.result = parseFloat(this.operand1) / parseFloat(this.operand2);
+                        this.display = parseFloat(this.result);
+                        break;
+                    default:
+
+                }
+            } else {
+                    switch (this.operator) {
+                        case '+' :
+                            this.result += parseFloat(this.operand2);
+                            this.display = parseFloat(this.result);
+                            break;
+
+                        case '-' :
+                            this.result -= parseFloat(this.operand2);
+                            this.display = parseFloat(this.result);
+                            break;
+
+                        case '*' :
+                            this.result *= parseFloat(this.operand2);
+                            this.display = parseFloat(this.result);
+                            break;
+
+                        case '/' :
+                            this.result /= parseFloat(this.operand2);
+                            this.display = parseFloat(this.result);
+                            break;
+                        default:
+                    }
+                }
+            }
+        ,
+            number: function (num) {
+                if (!this.operator) {
+                    this.operand1 += num;
+                    this.display = parseFloat(this.operand1);
+                } else {
+                    if (!this.result) {
+                        this.operand2 += num;
+                        this.display = parseFloat(this.operand2);
+                    } else {
+                        this.operand1 = this.result;
+                        this.result = null;
+                        this.operand2 = '0';
+                        this.operand2 += num;
+                        this.display = parseFloat(this.operand2);
+                    }
+                }
+            }
+        ,
+            setOperator: function (operator) {
+                this.operator = operator;
+                this.display = this.operator;
+            }
+        ,
+            reset: function () {
+                this.operator = null;
+                this.operand1 = '0';
+                this.operand2 = '0';
+                this.result = null;
+                this.display = '0';
+            }
         }
-    }
-});
+    });
